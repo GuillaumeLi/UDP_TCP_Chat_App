6868f4f0c44f2c45ea75ca19215e9a4d4b86dff3
jimmpanj40 <jimmy-antoine.pan@ensea.fr>
2022-10-28 11:31:42 +0200

    UDP SERVER done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.SocketException;
 
 public class Main {
-    public static void main (String args[]) throws IOException {
+    public static void main (String[] args) throws IOException {
         UDP_Server udp_server = new UDP_Server(Integer.parseInt(args[0]));
         udp_server.launch();
         /** With the command "nc -u localhost 8000" in a other terminal, we can connect to the server as client.**/

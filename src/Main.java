68c06c86ecccc5e2f51a46a359733e73145a433b
GuillaumeLi <81261803+GuillaumeLi@users.noreply.github.com>
2022-10-28 09:32:35 +0200

    Add of launch method in UDP_Server

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
-public class Main {
+import java.io.IOException;
+import java.net.SocketException;
 
+public class Main {
+    public static void main (String args[]) throws IOException {
+        UDP_Server udp_server = new UDP_Server(Integer.parseInt(args[0]));
+        udp_server.launch();
+    }
 }
--- src/UDP_Server.java
+++ src/UDP_Server.java
@@ -1,18 +1,55 @@
+import java.io.IOException;
+import java.net.*;
+
 public class UDP_Server {
 
     private int port;
+    private DatagramSocket udpSocket;
+
+    private boolean running;
 
     // Default constructor
-    public UDP_Server() {
+    public UDP_Server() throws SocketException {
         this(8000);
     }
 
-    public UDP_Server(int port) {
+    public UDP_Server(int port) throws SocketException {
         this.port = port;
+        udpSocket = new DatagramSocket(this.port);
     }
 
-    public launch() {
+    public void launch() throws IOException {
+        System.out.println("-- Running Server at " + InetAddress.getLocalHost() + "--");
+        String msg;
+
+        running = true;
+        toString();
+
+        while (running) {
+
+            byte[] buf = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+            // blocks until a packet is received
+            udpSocket.receive(packet);
+            msg = new String(packet.getData()).trim();
+
+            System.out.println(
+                    "Message from " + packet.getAddress().getHostAddress() + ": " + msg);
+        }
 
     }
 
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    @Override
+    public String toString() {
+        return "The server is running : " + running;
+    }
 }

3646c2def3fa208774a74d22035ccb1299d15483
GuillaumeLi <81261803+GuillaumeLi@users.noreply.github.com>
2022-10-28 08:51:36 +0200

    Creation of UDP_Server class

---
+++ src/UDP_Server.java
@@ -0,0 +1,18 @@
+public class UDP_Server {
+
+    private int port;
+
+    // Default constructor
+    public UDP_Server() {
+        this(8000);
+    }
+
+    public UDP_Server(int port) {
+        this.port = port;
+    }
+
+    public launch() {
+
+    }
+
+}
